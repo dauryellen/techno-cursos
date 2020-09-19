1a9071979c2a5a79c670f3402b681d1f9db0e3af
Daury Mendes <dauryellen@live.com>
2020-09-18 22:01:52 -0300

    header do app

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Contato from "../views/Contato.vue";
+import Cursos from "../views/Cursos.vue";
 
 Vue.use(VueRouter);
+
+const routes = [
+	{
+		path: "/",
+		name: "Home",
+		component: Home,
+	},
+	{
+		path: "/contato",
+		name: "Contato",
+		component: Contato,
+	},
+	{
+		path: "/cursos",
+		name: "Cursos",
+		component: Cursos,
+	},
+];
 
 const router = new VueRouter({
 	mode: "history",
